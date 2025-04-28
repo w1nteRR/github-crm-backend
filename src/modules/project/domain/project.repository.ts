28132f7492286881b9b6ab8366918cc9aa72c71f
w1nteRR <mykola796@gmail.com>
2025-04-29 02:09:18 +0300

    update project repository

--- src/modules/project/domain/project.repository.ts
+++ src/modules/project/domain/project.repository.ts
@@ -2,4 +2,6 @@ import { Project } from './entities/Project';
 
 export interface IProjectRepository {
   save(project: Project): Promise<void>;
+  delete(project_id: string): Promise<boolean>;
+  findById(project_id: string): Promise<Project | null>;
 }
