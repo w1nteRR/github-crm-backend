a123532ff5113ff580c22387e2e5c2243f185850
w1nteRR <mykola796@gmail.com>
2025-04-30 20:07:49 +0300

    init app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProjectModule } from './modules/project/project.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { JwtAuthGuard } from '@libs/auth/validate-token.guard';
 
 @Module({
-  imports: [CqrsModule.forRoot()],
+  imports: [
+    CqrsModule.forRoot(),
+    EventEmitterModule.forRoot(),
+    ProjectModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtAuthGuard],
 })
 export class AppModule {}
