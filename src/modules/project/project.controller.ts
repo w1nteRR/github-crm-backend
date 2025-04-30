b12d73e0c20280ef941dead6f285919dede93b6b
w1nteRR <mykola796@gmail.com>
2025-04-30 20:07:41 +0300

    add project controller

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -1,16 +1,22 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateProjectCommand } from './application/commands/create-project.command';
+import { JwtAuthGuard } from '@libs/auth/validate-token.guard';
+import { AuthenticatedRequest } from '@custom-types/network/network.types';
 
-@Controller('project')
+@Controller('api/v1/project')
 export class ProjectController {
   constructor(
     private readonly queryBus: QueryBus,
     private readonly commandBus: CommandBus,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  async createProject(@Body() { project_name }: { project_name: string }) {
+  async createProject(
+    @Req() req: AuthenticatedRequest,
+    @Body() { project_name }: { project_name: string },
+  ) {
     await this.commandBus.execute(new CreateProjectCommand(project_name));
   }
 }
