34b0d80bdf92356f2d58bb9bc6e5ce7f4c2969bf
w1nteRR <mykola796@gmail.com>
2025-04-30 20:04:59 +0300

    refactor

--- src/modules/auth/dto/auth-sign-up.schema.ts
+++ src/modules/auth/dto/auth-sign-up.schema.ts
@@ -4,11 +4,11 @@ export const authSignUpSchema = z
   .object({
     email: z.string().email(),
     password: z.string().min(8),
-    confirmPassword: z.string().min(8),
+    confirm_password: z.string().min(8),
   })
-  .refine((data) => data.password === data.confirmPassword, {
+  .refine((data) => data.password === data.confirm_password, {
     message: 'Passwords do not match',
-    path: ['confirmPassword'],
+    path: ['confirm_password'],
   });
 
 export type AuthSignUpDto = z.infer<typeof authSignUpSchema>;
