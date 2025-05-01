d85c2967670f4db020bd02ed961766f5ed7ccb35
w1nteRR <mykola796@gmail.com>
2025-05-01 18:24:35 +0300

    add a project repository

--- src/modules/project/domain/project.repository.ts
+++ src/modules/project/domain/project.repository.ts
@@ -1,7 +1,15 @@
 import { Project } from './entities/Project';
+import {
+  DeleteProjectProps,
+  GetProjectProps,
+  ProjectEditProps,
+  ProjectList,
+} from '../types/project.types';
 
 export interface IProjectRepository {
-  save(project: Project): Promise<void>;
-  delete(project_id: string): Promise<boolean>;
-  findById(project_id: string): Promise<Project | null>;
+  save(project: Project, user_id: string): Promise<void>;
+  delete(props: DeleteProjectProps): Promise<void>;
+  update(project: ProjectEditProps, user_id: string): Promise<Project>;
+  findById(props: GetProjectProps): Promise<Project | null>;
+  findMany(user_id: string): Promise<ProjectList>;
 }
--- src/modules/project/infrastructure/project-repository.impl.ts
+++ src/modules/project/infrastructure/project-repository.impl.ts
@@ -1,7 +1,61 @@
 import { IProjectRepository } from '../domain/project.repository';
+import { Project } from '../domain/entities/Project';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@libs/external/prisma/prisma.service';
+import { ProjectMapper } from '../project.mapper';
+import {
+  DeleteProjectProps,
+  GetProjectProps,
+  ProjectEditProps,
+  ProjectList,
+} from '../types/project.types';
+import { Project as PrismaProject } from '../../../../generated/prisma';
 
+@Injectable()
 export class ProjectRepositoryImpl implements IProjectRepository {
-  save(): Promise<void> {
-    return Promise.resolve(undefined);
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly mapper: ProjectMapper,
+  ) {}
+
+  public async save(project: Project, user_id: string): Promise<void> {
+    await this.prisma.project.create({
+      data: { ...this.mapper.fromDomainToPrisma(project), user_id },
+    });
+  }
+
+  public async delete(props: DeleteProjectProps): Promise<void> {
+    const { project_id, user_id } = props;
+    await this.prisma.project.delete({ where: { id: project_id, user_id } });
+  }
+
+  public async findById(props: GetProjectProps): Promise<Project | null> {
+    const { project_id, user_id } = props;
+
+    const project: PrismaProject | null = await this.prisma.project.findFirst({
+      where: { id: project_id, user_id },
+    });
+
+    if (!project) return null;
+
+    return this.mapper.fromPrismaToDomain(project);
+  }
+
+  public async findMany(user_id: string): Promise<ProjectList> {
+    return this.prisma.project.findMany({
+      where: { user_id },
+    });
+  }
+
+  public async update(
+    project: ProjectEditProps,
+    user_id: string,
+  ): Promise<Project> {
+    const updatedProject: PrismaProject = await this.prisma.project.update({
+      where: { id: project.id, user_id },
+      data: project,
+    });
+
+    return this.mapper.fromPrismaToDomain(updatedProject);
   }
 }
