e93c5d2ded03bcd28afaeba80b981be0404597c9
w1nteRR <mykola796@gmail.com>
2025-05-01 18:24:45 +0300

    update mapper

--- src/modules/project/project.mapper.ts
+++ src/modules/project/project.mapper.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Project } from './domain/entities/Project';
 import type { IGitHubProject } from '@custom-types/github/github.types';
+import { Project as PrismaProject } from '../../../generated/prisma';
 
 @Injectable()
 export class ProjectMapper {
@@ -16,6 +17,9 @@ export class ProjectMapper {
       created_at,
     } = gitHubProject;
 
+    const date = new Date(created_at);
+    const unix = Math.floor(date.getTime() / 1000);
+
     return new Project({
       id: String(id),
       props: {
@@ -25,8 +29,32 @@ export class ProjectMapper {
         stars: stargazers_count,
         forks: forks_count,
         issues: open_issues_count,
-        createdAt: created_at,
+        created_at: unix,
       },
+    });
+  }
+
+  public fromDomainToPrisma(project: Project) {
+    const { name, owner, url, created_at, stars, issues, forks } =
+      project.getProps();
+    const id = project.id;
+
+    return {
+      id,
+      name,
+      owner,
+      url,
+      stars,
+      forks,
+      issues,
+      created_at: Math.floor(created_at / 1000),
+    };
+  }
+
+  public fromPrismaToDomain(project: PrismaProject): Project {
+    return new Project({
+      id: project.id,
+      props: project,
     });
   }
 }
