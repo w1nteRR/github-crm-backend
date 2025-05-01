289c5e2379785bb62a55fd5acb4da411a7608d54
w1nteRR <mykola796@gmail.com>
2025-05-01 18:25:33 +0300

    refactor fetch github

--- src/modules/project/application/queries/handlers/fetch-project-query.handler.ts
+++ src/modules/project/application/queries/handlers/fetch-project-query.handler.ts
@@ -2,6 +2,7 @@ import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { FetchProjectQuery } from '../fetch-project.query';
 import { GithubApiService } from '@libs/external/github/github-api.service';
 import { IGitHubProject } from '@custom-types/github/github.types';
+import { NotFoundException } from '@nestjs/common';
 
 @QueryHandler(FetchProjectQuery)
 export class FetchProjectQueryHandler
@@ -12,6 +13,14 @@ export class FetchProjectQueryHandler
   public async execute(
     query: FetchProjectQuery,
   ): Promise<IGitHubProject | null> {
-    return await this.githubApiService.fetchProjectByName(query.project_name);
+    const result = await this.githubApiService.fetchProjectByName(
+      query.project_name,
+    );
+
+    if (!result.id) {
+      throw new NotFoundException('Project not found in GitHub');
+    }
+
+    return result;
   }
 }
